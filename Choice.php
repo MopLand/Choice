8e7f8561ff2aff95469dd699cc6824cd97bb8609
Lay <veryide@qq.com>
2018-07-31 14:04:14 +0800

    foramt!!

--- Choice.php
+++ Choice.php
@@ -4,13 +4,13 @@ class Choice{
 
 	//当前接口版本
 	private $Version = 1.0;
-	
+
 	//接口用户代理
-    private $Headers = array();
-    private $UserAgent = array();
-	
+	private $Headers = array();
+	private $UserAgent = array();
+
 	//接口网关地址
-    private $Gateway = 'http://gateway.shihuizhu.net/open/';
+	private $Gateway = 'http://gateway.shihuizhu.net/open/';
 
 	/**
 	* @desc 构造函数
@@ -18,42 +18,42 @@ class Choice{
 	* @param string  appkey
 	* @return void
 	*/
-    public function __construct( $AppID, $AppKey ){       
-       $this->header( $AppID, $AppKey );       
-    }
+	public function __construct( $AppID, $AppKey ){       
+		$this->header( $AppID, $AppKey );       
+	}
 
 	/**
 	* @desc 请求头生成
 	* @param string  appid
 	* @param string  appkey
-	* @return string
+	* @return array
 	*/
-    private function header( $AppID, $AppKey ){    
+	private function header( $AppID, $AppKey ){    
 		$this->UserAgent = array( 'VERSION' => $this->Version, 'APPID' => $AppID, 'APPKEY' => $AppKey );
-				
+
 		$headers = array();    
 		foreach( $this->UserAgent as $k => $v ){
 			$headers[] = strtoupper( $k ) . ':'. $v;
 		}
-		
+
 		return $this->Headers = $headers;    
-    }
+	}
 
 	/**
 	* @desc 调用方法
 	* @param string  URL 地址
 	* @return string
 	*/
-    private function fetch( $url ){
-        $ch = curl_init();        
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->Headers);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        return $result;
+	private function fetch( $url ){
+		$ch = curl_init();        
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $this->Headers);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		return $result;
 	}
 
 	/**
@@ -62,25 +62,25 @@ class Choice{
 	* @param string  可选参数
 	* @return string
 	*/
-    public function __call( $func, $opts = array() ){
-        
-        $parts = array( $func );
-        $param = array();
-        
-        foreach( $opts as $k => $v ){        
+	public function __call( $func, $opts = array() ){
+
+		$parts = array( $func );
+		$param = array();
+
+		foreach( $opts as $k => $v ){        
 			if( is_array( $v ) ){
 				$param = array_merge( $param, $v );
 			}else{
 				$parts[] = $v;
 			}        
-        }
-        
+		}
+
 		$route = implode('/', $parts );
 		$query = http_build_query( $param );
-        
-        $url = $this->Gateway . $route . ( $query ? '?' . $query : '' );
-        
-        return $this->fetch( $url );
-    }
+
+		$url = $this->Gateway . $route . ( $query ? '?' . $query : '' );
+
+		return $this->fetch( $url );
+	}
 
 }
